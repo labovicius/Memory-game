f446ba256feb8e95c4b38ac8fac8392684fd63a7
Bozo Labovic <bozolabovic7@gmail.com>
2021-11-06 01:36:50 +0100

    init commit

---
+++ game.js
@@ -0,0 +1,201 @@
+let configuration = {
+  gameStarted: false,
+  checkingCards: false,
+  min: 0,
+  sec: 0,
+  remaining: 16,
+  completed: 0,
+  pictures: [
+    "bm-logo",
+    "pumpkin",
+    "bats",
+    "witch",
+    "bm-text",
+    "dresscode",
+    "cat",
+    "castle",
+  ],
+};
+
+let guessedIndex = [];
+let cards = [];
+let deleteNodes = [];
+let combination = [];
+
+const timerElement = document.getElementsByClassName(`timer`);
+const scoreElement = document.getElementsByClassName(`score`);
+
+let stopwatch;
+
+startTimer = () => {
+  stopwatch = setInterval(function () {
+    if (++configuration.sec > 59) {
+      configuration.sec -= 60;
+      ++configuration.min;
+    }
+    if (configuration.sec < 10) {
+      timerElement[0].innerHTML =
+        "0" + configuration.min + ":0" + configuration.sec;
+    } else {
+      timerElement[0].innerHTML =
+        "0" + configuration.min + ":" + configuration.sec;
+    }
+
+    if (configuration.min === 5) {
+      clearInterval(stopwatch);
+    }
+  }, 1000);
+};
+
+startGame = () => {
+  configuration.gameStarted = true;
+
+  clearInterval(stopwatch);
+  startTimer();
+
+  guessedIndex = [];
+
+  configuration.min = 0;
+  configuration.sec = 0;
+  configuration.remaining = 16;
+  configuration.completed = 0;
+
+  timerElement[0].style.visibility = "visible";
+  scoreElement[0].innerHTML =
+    configuration.completed + "/" + configuration.remaining;
+  scoreElement[0].style.visibility = "visible";
+
+  deleteNodes.forEach((element) => {
+    removeElementStyle(element.style);
+  });
+
+  combination = generateCombination.publicGen();
+};
+
+addPoints = () => {
+  // CLICKED ON THE GUESSED CARD
+  for (let i = 0; i < guessedIndex.length; i++) {
+    if (guessedIndex[i] === cards[0].elem) {
+      cards = [];
+      configuration.checkingCards = false;
+      return;
+    }
+  }
+
+  // CLICKED ON THE SAME CARD
+  if (cards[0].row === cards[1].row && cards[0].ind === cards[1].ind) {
+    cards.shift();
+    configuration.checkingCards = false;
+    return;
+  }
+
+  // AFTER 1000 ms REMOVE STYLE FROM WRONG CARDS
+  if (cards[0].elem !== cards[1].elem) {
+    let timer = setTimeout(() => {
+      let fElem = document.getElementsByClassName(`row${cards[0].row}`)[0];
+      let sElem = document.getElementsByClassName(`row${cards[1].row}`)[0];
+
+      let firstElem = fElem.getElementsByClassName(`div${cards[0].ind}`)[0]
+        .style;
+      let secondElem = sElem.getElementsByClassName(`div${cards[1].ind}`)[0]
+        .style;
+
+      removeElementStyle(firstElem);
+      removeElementStyle(secondElem);
+
+      cards = [];
+      configuration.checkingCards = false;
+    }, 1000);
+  } else if (cards[0].elem === cards[1].elem) {
+    guessedIndex.push(cards[0].elem);
+
+    configuration.remaining -= 2;
+    configuration.completed += 2;
+    scoreElement[0].innerHTML = configuration.completed + "/16";
+
+    cards = [];
+    configuration.checkingCards = false;
+  }
+};
+
+addElementToCard = (elem, ind, row) => {
+  let card = {
+    elem,
+    ind,
+    row,
+  };
+
+  const r = document.getElementsByClassName(`row${row}`);
+
+  cards.push(card);
+
+  deleteNodes.push(r[0].getElementsByClassName(`div${card.ind}`)[0]);
+
+  let picture = configuration.pictures[elem - 1];
+
+  let row_pic = document.getElementsByClassName(`row${row}`)[0];
+
+  setElementStyle(
+    row_pic.getElementsByClassName(`div${ind}`)[0].style,
+    picture
+  );
+};
+
+removeElementStyle = (element) => {
+  element.backgroundImage = "none";
+  element.backgroundColor = "#f98a00";
+  element.borderTopRightRadius = "0px";
+  element.borderBottomLeftRadius = "0px";
+  element.borderTopLeftRadius = "40px";
+  element.borderBottomRightRadius = "40px";
+  element.boxShadow = "10px 15px #272324";
+};
+
+setElementStyle = (element, picture) => {
+  element.backgroundColor = "#272324";
+  element.backgroundImage = `url(assets/${picture}.jpg)`;
+  element.borderTopRightRadius = "40px";
+  element.borderBottomLeftRadius = "40px";
+  element.borderTopLeftRadius = "0px";
+  element.borderBottomRightRadius = "0px";
+  element.boxShadow = "10px 15px #f98a00";
+};
+
+checkCardBox = (elem, ind, row) => {
+  if (configuration.gameStarted && !configuration.checkingCards) {
+    addElementToCard(elem, ind, row);
+
+    for (let i = 0; i < guessedIndex.length; i++) {
+      if (guessedIndex[i] === cards[cards.length - 1].elem) {
+        cards.pop();
+        return;
+      }
+    }
+
+    if (cards.length === 2) {
+      configuration.checkingCards = true;
+      addPoints();
+    }
+
+    if (configuration.remaining === 0) {
+      clearInterval(stopwatch);
+    }
+  }
+};
+
+generateCombination = (function () {
+  let privateGen = function () {
+    let combination = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    for (let i = combination.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [combination[i], combination[j]] = [combination[j], combination[i]];
+    }
+    return combination;
+  };
+
+  return {
+    publicGen: function () {
+      return privateGen();
+    },
+  };
+})();
---
+++ script.js
@@ -0,0 +1,38 @@
+const startButton = document.getElementsByClassName("startGame")[0];
+const cardHolder = document.getElementsByClassName("card-holder");
+
+(function () {
+  for (let i = 0; i < 4; i++) {
+    let div = document.createElement("div");
+    div.classList.add(`row${i + 1}`);
+    cardHolder[0].appendChild(div);
+
+    let row = document.getElementsByClassName(`row${i + 1}`)[0];
+
+    for (let j = 0; j < 4; j++) {
+      let div = document.createElement("div");
+      div.classList.add(`div${j + 1}`);
+      row.appendChild(div);
+    }
+  }
+})();
+
+startButton.addEventListener("click", function () {
+  startGame();
+});
+
+Array.from(cardHolder).forEach((holder) => {
+  holder.childNodes.forEach((row) => {
+    row.childNodes.forEach((element) => {
+      let current_row = parseInt(row.classList.value.slice(-1));
+      element.addEventListener("click", () => {
+        let id = parseInt(element.classList.value.slice(-1));
+        checkCardBox(
+          combination[(current_row - 1) * 4 + (id - 1)],
+          id,
+          current_row
+        );
+      });
+    });
+  });
+});
